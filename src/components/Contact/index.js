649a6af46f93b8bb73f32355c484f712b206f01f
Sam M <62692646+seneka13@users.noreply.github.com>
2020-05-20 17:05:54 +0600

    about page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import Nav from './Nav/Nav';
-
-
+import Nav from '../Nav';
+import styles from './contact.module.scss';
 
 
 function Contact() {
     return (
     <div>
         <Nav/>
-        <h1>
+        <h1 className={styles['clas']}>
             Contact
         </h1>
     </div>
-
     );
     }
-
 
     export default Contact
