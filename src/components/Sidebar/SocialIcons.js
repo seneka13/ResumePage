ff0231f5838bad67df446951546293317cede722
Sam M <62692646+seneka13@users.noreply.github.com>
2020-05-29 17:42:22 +0600

    rewrite last mistakes

--- src/components/Sidebar/SocialIcons.js
+++ src/components/Sidebar/SocialIcons.js
@@ -7,28 +7,18 @@ import styles from "./sidebar.module.scss";
 
 function SocialIcons() {
 
+    const socialLink = [ 
+        {id: 1, social: <Facebook className={styles['social-icon']}/>, href:'https://www.facebook.com/profile.php?id=100024608370678'},
+        {id: 2, social: <Github className={styles['social-icon']}/>, href:'https://github.com/seneka13'},
+        {id: 3, social: <Telegram className={styles['social-icon']}/>, href:'https://telegram.org//'},
+        {id: 4, social: <Whatsapp className={styles['social-icon']}/>, href:'https://www.whatsapp.com/'}
+    ]
+
     return (
         <div className={styles.social}>
-            <a href="https://www.facebook.com/profile.php?id=100024608370678"
-            target='_blank'
-            rel="noopener noreferrer">
-                <Facebook  className={styles['social-icon']} />
-            </a>
-            <a href="https://github.com/seneka13" 
-            target='_blank'
-            rel="noopener noreferrer">
-                <Github  className={styles['social-icon']}/>
-            </a>
-            <a href="https://telegram.org//" 
-            target='_blank'
-            rel="noopener noreferrer">
-                <Telegram className={styles['social-icon']}/>
-            </a>
-            <a href="https://www.whatsapp.com/" 
-            target='_blank'
-            rel="noopener noreferrer">
-                <Whatsapp  className={styles['social-icon']}/>
-            </a>
+            {socialLink.map(item => (
+                <a key={item.id} href={item.href} target='_blank' rel="noreferrer noopener">{item.social}</a>
+            ))}
         </div>
     );
 }
