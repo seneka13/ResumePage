16eb9b64994bd8db11eba88cbb12a26b52a51b8e
Sam M <62692646+seneka13@users.noreply.github.com>
2020-06-15 12:11:53 +0600

    Updates

---
+++ precache-manifest.8dce1ca582f6609fd668e75b1ac4cdfd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4585451f5046514708709d08476b7e1a",
+    "url": "/ResumePage/index.html"
+  },
+  {
+    "revision": "bf2de0521ecf3bbd3ee4",
+    "url": "/ResumePage/static/css/2.42836a8c.chunk.css"
+  },
+  {
+    "revision": "53a03ba1cf28463e6af7",
+    "url": "/ResumePage/static/css/main.f3ec4b01.chunk.css"
+  },
+  {
+    "revision": "bf2de0521ecf3bbd3ee4",
+    "url": "/ResumePage/static/js/2.050d858a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ResumePage/static/js/2.050d858a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53a03ba1cf28463e6af7",
+    "url": "/ResumePage/static/js/main.6ee9d5ce.chunk.js"
+  },
+  {
+    "revision": "709211295984dde7379a",
+    "url": "/ResumePage/static/js/runtime-main.ad3477bb.js"
+  },
+  {
+    "revision": "d130ad5393f2b26ccb35d0a92fb6f47b",
+    "url": "/ResumePage/static/media/Samj.d130ad53.jpg"
+  },
+  {
+    "revision": "06471bc39787ea52f748e22a6641386a",
+    "url": "/ResumePage/static/media/contact.06471bc3.jpg"
+  },
+  {
+    "revision": "d4fb92ac2fc97de9490d62f235aca64c",
+    "url": "/ResumePage/static/media/portrait.d4fb92ac.png"
+  },
+  {
+    "revision": "620de2cd6084f4123542cca7630e5056",
+    "url": "/ResumePage/static/media/sam.620de2cd.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ResumePage/precache-manifest.0b54ef0de68b9ed400313aa176f8f8f1.js"
+  "/ResumePage/precache-manifest.8dce1ca582f6609fd668e75b1ac4cdfd.js"
 );
 
 self.addEventListener('message', (event) => {
