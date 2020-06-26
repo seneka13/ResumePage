df7d66432899ad69ead07935c42010cea1e3760b
Sam M <62692646+seneka13@users.noreply.github.com>
2020-06-27 01:06:58 +0600

    add link for portfolio

--- src/components/SafeCity/index.jsx
+++ src/components/SafeCity/index.jsx
@@ -4,15 +4,14 @@ import PageWrapper from '../PageWrapper'
 import style from './safe.module.scss'
 
 
-
 function SafeCity() {
   return (
     <PageWrapper>
       <Container>
         <Row className="pt-5">
           <Col xs={12}>
-          <div className={style.text}>Сайт Охранного агентства "Безопасный город"</div>
-          <a className={style.link} href="http://134.209.252.196/" target="_blank" rel="noreferrer noopener">Посмотреть сайт</a>
+            <div className={style.text}>Сайт Охранного агентства "Безопасный город"</div>
+            <a className={style.link} href="http://safecity.kg/" target="_blank" rel="noreferrer noopener">Посмотреть сайт</a>
           </Col>
         </Row>
       </Container>
