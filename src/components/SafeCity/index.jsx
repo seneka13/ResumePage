0cd82d95b370a7abaf05bfe9faf705843db6947e
Sam M <62692646+seneka13@users.noreply.github.com>
2020-07-24 00:48:23 +0600

    change portfolio img

--- src/components/SafeCity/index.jsx
+++ src/components/SafeCity/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import PageWrapper from '../PageWrapper'
 import style from './safe.module.scss'
+import safe from '../../img/safecity.png'
 
 
 function SafeCity() {
@@ -10,9 +11,10 @@ function SafeCity() {
       <div>
         <Container>
           <Row className="pt-5">
-            <Col xs={12}>
+            <Col xs={12} className="d-flex flex-column align-items-center">
               <div className={style.text}>Сайт Охранного агентства "Безопасный город"</div>
               <a className={style.link} href="http://safecity.kg/" target="_blank" rel="noreferrer noopener">Посмотреть сайт</a>
+              <img className={style.img} src={safe} alt="safecity" />
             </Col>
           </Row>
         </Container>
--- src/components/ShareYSelf/index.jsx
+++ src/components/ShareYSelf/index.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import PageWrapper from '../PageWrapper'
+import share from '../../img/share.png'
 import style from './share.module.scss'
 
 
 function ShareYSelf() {
   return (
+
     <PageWrapper>
-      <Container>
-        <Row className="pt-5">
-          <Col xs={12}>
-            <div className={style.text}>Веб-сервис ShareYSelf</div>
-            <a className={style.link} href="https://seneka13.github.io/ShareYSelf-project/" target="_blank" rel="noreferrer noopener">Посмотреть сайт</a>
-          </Col>
-        </Row>
-      </Container>
+      <div>
+        <Container>
+          <Row className="pt-5">
+            <Col xs={12} className="d-flex flex-column align-items-center">
+              <div className={style.text}>Сайт Охранного агентства "Безопасный город"</div>
+              <a className={style.link} href="https://seneka13.github.io/ShareYSelf-project/" target="_blank" rel="noreferrer noopener">Посмотреть сайт</a>
+              <img className={style.img} src={share} alt="safecity" />
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </PageWrapper>
   )
 }
